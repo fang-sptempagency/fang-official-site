3950c06c95586cd8ddb55daf568158097c9e23b6
ink. <notinkbutink@gmail.com>
2026-03-17 05:57:55 +0900

    feat: :tada: first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="public/images/Fang._logo_white.svg">
+  <source media="(prefers-color-scheme: light)" srcset="public/images/Fang._logo_black.svg">
+  <img alt="Fang." src="public/images/Fang._logo_black.svg">
+</picture>
+
+# ABOUT
+一次創作サイト「Fang.」のソースコードです。
+
+本サイトはHTMLのみで構成し、CSSを付与しません。
+
+演出上、動的な表現が必要なUIのみJavascriptを導入します。
+
+- Author / Developer : [ink.](https://linktr.ee/notinkbutink)
+- Develop Diary : [Zenn scrap](https://zenn.dev/notinkbutink/scraps/23b2ee6646fa74)
+- Licence : [CC BY-NC-ND 4.0](https://creativecommons.org/licenses/by-nc-nd/4.0/deed.ja)
+
+# VERSION CONTROL
+```
+Fang._ver.X.Y.Z
+```
+- X : コンセプト変更時
+- Y : 見出し1レベルのカテゴリ追加時
+- Z : TBD
+
+# TODO
+## MUST
+- [ ] 世界線作図
+- [ ] 人物デザイン作図
+- [ ] 人物配色確定
+- [ ] 可視光スペクトル図示
+- [ ] 特殊能力仕様定義
+
+## WANT
+- [ ] ダークモード
+- [ ] タグ選択
+- [ ] サムネイル表示~~（Pixivに怒られそう）~~
---
+++ public/sw.js
@@ -0,0 +1,40 @@
+// キャッシュ名にバージョンを含める（更新時に重要）
+const CACHE_NAME = '20260309 21:37';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+navigator.serviceWorker.register('/sw.js').then(reg => {
+  // 起動時に明示的にアップデートを確認
+  reg.update(); 
+  
+  reg.onupdatefound = () => {
+    const newWorker = reg.installing;
+    newWorker.onstatechange = () => {
+      if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+        // ここで「新しいバージョンがあります。再起動しますか？」等のUIを出す
+        if (confirm('新しいバージョンが利用可能です。更新しますか？')) {
+          location.reload();
+        }
+      }
+    };
+  };
+});
+
+self.addEventListener('install', (event) => {
+  // 待機状態をスキップして、すぐに有効化させる
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // 古いキャッシュを削除し、すぐに全クライアント（タブ）を制御下に置く
+  event.waitUntil(clients.claim());
+});
