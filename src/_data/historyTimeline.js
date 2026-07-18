0b671f584e1b71e0233fa287f8f3541a42c3c1ba
ink. <notinkbutink@gmail.com>
2026-07-19 07:19:38 +0900

    feat: 現代史の内容補強
    戸次「うわーなんか増えた」
    戸次「わかんねーー」
    戸次「...ん？」
    
    戸次「週刊マイ3Dプリンター？？」
    戸次「なにこれ」
    戸次「あ！わかった」
    戸次「ディアゴスティー二だ」
    戸次「テレビでみた」
    戸次「知ってた？」
    篠原「知りませんでした。」
    篠原「ディアゴスティー二がなんなのかも知りません。」
    戸次「なんかオマケが毎回つく雑誌ー」
    戸次「"小学1年生"みたいなヤツ。」
    篠原「...そうなんですか」
    篠原は娯楽の雑誌に触れさせてもらえたことがなかった...。

--- src/_data/historyTimeline.js
+++ src/_data/historyTimeline.js
@@ -35,14 +35,26 @@ module.exports = {
 
   events: [
     {
-      year: 1945,
+      date: "1945-08-06",
+      title: "広島市への原子爆弾投下",
+      type: "history",
+      side: "real"
+    },
+    {
+      date: "1945-08-09",
+      title: "長崎市への原子爆弾投下",
+      type: "history",
+      side: "real"
+    },
+    {
+      date: "1945-08-15",
       title: "終戦",
       type: "history",
       side: "real"
     },
     {
-      year: 1964,
-      title: "東京オリンピック",
+      date: "1954-03-01",
+      title: "ブラボー実験により第五福竜丸被曝",
       type: "history",
       side: "real"
     },
@@ -53,16 +65,52 @@ module.exports = {
       side: "fang"
     },
     {
+      date: "1964-10-10",
+      title: "東京オリンピック",
+      type: "history",
+      side: "real"
+    },
+    {
+      year: 1964,
+      title: "北条時子 誕生",
+      type: "birth",
+      side: "fang"
+    },
+    {
       year: 1970,
       title: "葛城正親 誕生",
       type: "birth",
       side: "fang"
     },
     {
+      date: "1970-03-15",
+      title: "日本万国博覧会",
+      type: "history",
+      side: "real"
+    },
+    {
+      year: 1982,
+      title: "延金英一（27才）、葛城正親（12才）を残して時間凍結される",
+      type: "history",
+      side: "fang"
+    },
+    {
+      date: "1983-07-15",
+      title: "任天堂 ファミリーコンピュータ発売",
+      type: "history",
+      side: "real"
+    },
+    {
       year: 1985,
       title: "指宿楓 誕生",
       type: "birth",
       side: "fang"
+    },
+    {
+      date: "1985-08-12",
+      title: "日本航空123便墜落事故",
+      type: "history",
+      side: "real"
     },
     {
       date: "1989-01-07",
@@ -86,9 +134,15 @@ module.exports = {
     },
     {
       date: "1994-05-05",
-      title: "葛城堅 誕生、葛城時子 死亡",
+      title: "葛城堅 誕生、母 時子（30才）死亡",
       type: "birth",
       side: "fang"
+    },
+    {
+      date: "1995-01-17",
+      title: "阪神・淡路大震災",
+      type: "history",
+      side: "real"
     },
     {
       date: "1995-03-20",
@@ -119,6 +173,13 @@ module.exports = {
       title: "本田千和 誕生",
       type: "birth"
     },
+
+    {
+      date: "1998-07-25",
+      title: "Microsoft Windows98 日本語版発売",
+      type: "history",
+      side: "real"
+    },
     {
       year: 2000,
       title: "横島透 誕生",
@@ -126,8 +187,32 @@ module.exports = {
       side: "fang"
     },
     {
+      date: "2001-06-08",
+      title: "附属池田小事件",
+      type: "history",
+      side: "real"
+    },
+    {
       date: "2001-09-11",
       title: "アメリカ同時多発テロ事件",
+      type: "history",
+      side: "real"
+    },
+    {
+      date: "2004-06-01",
+      title: "佐世保小6女児同級生殺害事件",
+      type: "history",
+      side: "real"
+    },
+    {
+      year: 2004,
+      title: "葛城堅（10才）が割腹自殺を試みて緊急搬送され、のち児童養護施設に保護される",
+      type: "fang",
+      side: "fang"
+    },
+    {
+      date: "2005-04-25",
+      title: "JR福知山線脱線事故",
       type: "history",
       side: "real"
     },
@@ -141,35 +226,69 @@ module.exports = {
       year: 2011,
       month: 3,
       day: 30,
-      title: "延金英一、時間凍結から解放される",
-      type: "history",
+      title: "延金英一（27才）が時間凍結から解放され、葛城正親（41才）に保護される",
+      type: "fang",
       side: "fang"
     },
     {
-      year: 2016,
-      month: 5,
-      day: 5,
-      title: "戸次信長、FANGの斡旋によるバイト初日にて篠原堅と邂逅",
+      date: "2015-01-15",
+      title: "「週刊 マイ3Dプリンター」創刊",
       type: "history",
+      side: "real"
+    },
+    {
+      date: "2016-05-05",
+      title: "戸次信長（20才）、FANGの斡旋によるバイト初日にて篠原堅（22才）と邂逅",
+      type: "fang",
       side: "fang"
+    },
+    {
+      date: "2016-07-26",
+      title: "相模原障害者施設殺傷事件",
+      type: "history",
+      side: "real"
     },
     {
       date: "2017-12-31",
-      title: "戸次信長、失踪",
-      type: "history",
+      title: "戸次信長（21才）失踪",
+      type: "fang",
       side: "fang"
     },
     {
       date: "2019-05-01",
       title: "令和開始",
       type: "history",
+      side: "real"
+    },
+    {
+      year: 2020,
+      title: "新型コロナウイルス感染症の流行",
+      type: "history",
+      side: "real"
+    },
+    {
+      date: "2020-04-18",
+      title: "葛城堅（25才）が本田千和（22才）と入籍し、苗字を本田に変える",
+      type: "fang",
       side: "fang"
     },
     {
-      date: "2026-01-16",
-      title: "戸次信長、帰還",
+      date: "2022-02-24",
+      title: "ロシアのウクライナ侵攻",
       type: "history",
+      side: "real"
+    },
+    {
+      date: "2026-01-16",
+      title: "戸次信長（30才）が帰還し、本田堅（31才）と本田千和（27才）に保護される",
+      type: "fang",
       side: "fang"
+    },
+    {
+      date: "2026-02-28",
+      title: "イスラエルとアメリカによるイラン攻撃",
+      type: "history",
+      side: "real"
     },
   ]
 };
